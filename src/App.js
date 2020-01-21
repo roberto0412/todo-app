abae5fc6c8c9aae272001d109bd063a89200adb3
roberto <roberto_esquivel04@outlook.com>
2020-01-21 05:22:41 -0800

    working initial fase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Todos from './Todos'
+
 
 function App() {
+  state = {
+    todos: [
+      {id:1, content: 'buy some milk'},
+      {id:2, content: 'play mario kart'}
+    ]
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="center blue-text">Todos's</h1>
+      <Todos Todos={this.state.todos}/>
+     
     </div>
   );
 }
---
+++ src/Todos.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+
+
+
+const Todos = ({todos}) => {
+
+    const todoList = todos.length ? (todos.map(todo => {
+        return (
+            <div className="collection item" key= {todo.id}>
+                <span>{todo.content}</span>
+            </div>
+        )
+    })) : (
+        <p className="center"> You have no todos left yay1</p>
+    )
+    return (
+
+        <div className="todos collection">
+            {todoList}
+
+        </div>
+    )
+}
+export default Todos
+
+
+
+
+
+
+
+
+
