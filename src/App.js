d3b9aee1555ecede7ac1efbbd9ef703bbc0bfb6d
roberto <roberto_esquivel04@outlook.com>
2020-01-21 17:00:40 -0800

    added functions to add and delete task

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import React, { Component } from 'react';
 import Todos from './Todos'
+import AddTodo from './AddTodo'
 
 
 class App extends Component {
@@ -20,12 +21,21 @@ class App extends Component {
   
     
   }
+  addTodo = (todo) =>{
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos
+    })
+
+  }
  
   render() {
     return (
       <div className="todo-app container">
         <h1 className="center blue-text">Todo's</h1>
         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTodo addTodo={this.addTodo}/>
       </div>
     );
   }
