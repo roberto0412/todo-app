9b52fc50a6ef148d5a7965bf330064be9701937e
roberto <roberto_esquivel04@outlook.com>
2020-01-21 16:26:30 -0800

    added functions for removing items from the list, updated logic

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import React from 'react';
+
+import React, { Component } from 'react';
 import Todos from './Todos'
 
 
-function App() {
+class App extends Component {
   state = {
     todos: [
-      {id:1, content: 'buy some milk'},
-      {id:2, content: 'play mario kart'}
+      {id: 1, content: 'buy some milk'},
+      {id: 2, content: 'play mario kart'}
     ]
   }
-  return (
-    <div className="App">
-      <h1 className="center blue-text">Todos's</h1>
-      <Todos Todos={this.state.todos}/>
-     
-    </div>
-  );
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id 
+    });
+    this.setState({
+      todos
+    })
+  
+    
+  }
+ 
+  render() {
+    return (
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+      </div>
+    );
+  }
 }
 
 export default App;
